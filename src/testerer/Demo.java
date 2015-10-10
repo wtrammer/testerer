8f0a52f396329257208137c045dfcacc222a4340
wtrammer <wolfgang.trammer@nts.eu>
2015-10-10 14:07:03 +0200

    demo

---
+++ src/testerer/Demo.java
@@ -0,0 +1,29 @@
+package testerer;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+int wert = 60;
+		
+		int i = 2;
+		
+		while (i <=9)
+		{
+			wert = wert * i;
+			System.out.println(wert);
+			i++;
+			
+		}
+		i=2;
+		while (i <=9)
+		{
+			wert = wert / i;
+			System.out.println(wert);
+			i++;
+		}
+	}
+
+}
